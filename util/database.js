99ed37500b9386fd9e5c78979a52ab597ca48273
MohammadReza Ranjbar <m.ranjbar80@gmail.com>
2022-01-27 22:55:41 +0330

    add database credentials

--- util/database.js
+++ util/database.js
@@ -4,12 +4,15 @@ const winston = require('winston')
 module.exports = class DB {
 
     constructor() {
-        this.url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
+        if (process.env.DB_USERNAME && process.env.DB_PASSWORD)
+            this.url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
+        else
+            this.url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
         this.name = process.env.DB_NAME
         this.client = new MongoClient(this.url);
     }
 
-    async get(){
+    async get() {
         await this.client.connect()
         return this.client.db(this.name);
     }
