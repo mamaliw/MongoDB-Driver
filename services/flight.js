bb8760e6e0f9eb073f924201e7c3319df56fad50
MohammadReza Ranjbar <m.ranjbar80@gmail.com>
2022-01-28 13:58:31 +0330

    Revert "v2"
    This reverts commit 43000194a1b78b0016b3ae3945d805c1901c715f.

--- services/flight.js
+++ services/flight.js
@@ -2,22 +2,34 @@ const mongodb = require('mongodb')
 const collection = DB.collection('flights')
 module.exports = {
 
-    async save(data) {
-
-        data.departureDate = new Date(data.departureDate)
-        data.price = parseInt(data.price)
-        data.capacity = parseInt(data.capacity)
-        return await collection.insertOne(data)
-
+    async save(data){
+        try {
+            data.departureDate = new Date(data.departureDate)
+            return await collection.insertOne(data)
+        }catch (e) {
+            log.error(e.status)
+        }
     },
-    async get(filter) {
-        return await collection.find(filter).toArray()
+    async get(filter){
+        try {
+            return await collection.find(filter).toArray()
+        }catch (e) {
+            log.error(e.status)
+        }
     },
-    async aggregate(query) {
-        return await collection.aggregate(query).toArray()
+    async aggregate(query){
+        try {
+            return await collection.aggregate(query).toArray()
+        }catch (e) {
+            log.error(e.status)
+        }
     },
-    async update(filter, update) {
-        return await collection.updateOne(filter, update)
+    async update(filter,update){
+        try {
+            return await collection.updateOne(filter,update)
+        }catch (e) {
+            log.error(e.status)
+        }
     },
 
 }
