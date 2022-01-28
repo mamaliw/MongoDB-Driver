d6f912b879948907d9ae590cb5aec407d7a72e25
MohammadReza Ranjbar <m.ranjbar80@gmail.com>
2022-01-28 18:49:51 +0330

    v2

--- router/flight.js
+++ router/flight.js
@@ -3,9 +3,60 @@ const flightsServices = require('../services/flight')
 //--------------------------------
 router.get('/', async (req, res) => {
     try {
-        const data = await flightsServices.get({})
-        console.log(req.query)
-        res.render('home', {data,filters:req.query})
+        log.info('/ query: ', req.query)
+        const query = []
+        if (req.query.origin) {
+            query.push({
+                $match: {
+                    origin: new RegExp(["^", req.query.origin, "$"].join(""), "i")
+                }
+            })
+        }
+        if (req.query.destination) {
+            query.push({
+                $match: {
+                    destination: new RegExp(["^", req.query.destination, "$"].join(""), "i")
+                }
+            })
+        }
+        if (req.query.minPrice) {
+            query.push({
+                $match: {
+                    price: {$gte: parseInt(req.query.minPrice)}
+                }
+            })
+        }
+        if (req.query.maxPrice) {
+            query.push({
+                $match: {
+                    price: {$lte: parseInt(req.query.maxPrice)}
+                }
+            })
+        }
+        if (req.query.flightClass && req.query.flightClass !== 'All') {
+            query.push({
+                $match: {
+                    flightType: new RegExp(["^", req.query.flightClass, "$"].join(""), "i")
+                }
+            })
+        }
+        if (req.query.company) {
+            query.push({
+                $match: {
+                    company: new RegExp(["^", req.query.company, "$"].join(""), "i")
+                }
+            })
+        }
+        if (req.query.departureTime) {
+            req.query.departureTime = new Date(req.query.departureTime)
+            query.push({
+                $match: {
+                    departureDate: req.query.departureTime
+                }
+            })
+        }
+        const data = await flightsServices.aggregate(query)
+        res.render('home', {data, filters: req.query})
     } catch (e) {
         log.error(e.stack)
         res.status(400).send(e.message)
@@ -23,8 +74,11 @@ router.post('/test', async (req, res) => {
 //--------------------------------
 router.post('/flight', async (req, res) => {
     try {
+        if (typeof parseInt(req.body.price) !== "number" || typeof parseInt(req.body.capacity) !== "number" || !(/\d\d\d\d-\d\d-\d\d/.test(req.body.departureDate))){
+            res.status(400).send('Bad Input')
+        }
         await flightsServices.save(req.body)
-        res.send('OK')
+        res.status(201).send('OK')
     } catch (e) {
         log.error(e.stack)
         res.status(400).send(e.message)
